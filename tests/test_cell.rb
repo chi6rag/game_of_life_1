5679a60d19f9027c99b371e2f0b916d95f5b2732
Chirag Aggarwal <me@chi6rag.net>
2015-10-26 14:26:01 +0530

    Adding expectations for #is_neighbour test on Cell

--- tests/test_cell.rb
+++ tests/test_cell.rb
@@ -10,4 +10,16 @@ class CellTest < Test::Unit::TestCase
 
   def test_new_with_invalid_coordinates_raises_error
   end
+
+  def test_is_neighbour_for_neighbouring_cell_returns_true
+    cell = GameOfLife::Cell.new(0,0, true)
+    neighbouring_cell = GameOfLife::Cell.new(1,0, true)
+    assert_true cell.is_neighbour? neighbouring_cell
+  end
+
+  def test_is_neighbour_for_non_neighbouring_cell_returns_true
+    cell = GameOfLife::Cell.new(0,0, true)
+    neighbouring_cell = GameOfLife::Cell.new(2,2, true)
+    assert_false cell.is_neighbour? neighbouring_cell
+  end
 end
