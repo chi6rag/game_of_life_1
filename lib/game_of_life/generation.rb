6ca8dcf7f149945485776b974f3ca4880201e747
Chirag Aggarwal <me@chi6rag.net>
2015-10-26 13:10:48 +0530

    Adding Generation class Adding live_cells method

---
+++ lib/game_of_life/generation.rb
@@ -0,0 +1,12 @@
+module GameOfLife
+  class Generation
+    def initialize(grid)
+      @grid = grid
+    end
+
+    def live_cells
+      0
+    end
+
+  end
+end
--- tests/test_generation.rb
+++ tests/test_generation.rb
@@ -5,6 +5,6 @@ class GenerationTest < Test::Unit::TestCase
   def test_new_with_valid_grid_returns_new_generation_with_no_live_cells
     grid = GameOfLife::Grid.new(3,3)
     generation = GameOfLife::Generation.new(grid)
-    assert_equals generation.live_cells, 0
+    assert_equal generation.live_cells, 0
   end
 end
