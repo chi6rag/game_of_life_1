32ff6a46387bb18447eeef3ca66f44fad7ba547f
Chirag Aggarwal <me@chi6rag.net>
2015-10-26 13:29:31 +0530

    Adding #next method for generation

--- tests/test_generation.rb
+++ tests/test_generation.rb
@@ -11,13 +11,20 @@ class GenerationTest < Test::Unit::TestCase
   def test_live_cells_for_generation_with_existent_live_cells_returns_live_cells
     grid = GameOfLife::Grid.new(3,3)
     live_cells = [
-      GameOfLife::Cell.new(0,0),
-      GameOfLife::Cell.new(0,1),
-      GameOfLife::Cell.new(0,2)
+      GameOfLife::Cell.new(0,0, true),
+      GameOfLife::Cell.new(0,1, true),
+      GameOfLife::Cell.new(0,2, true)
     ]
     generation = GameOfLife::Generation.new(grid, live_cells)
     assert_equal generation.live_cells.count, 3
-    assert_equal generation.live_cells.x_coordinate, 0
-    assert_equal generation.live_cells.y_coordinate, 0
+    assert_equal generation.live_cells.first.x_coordinate, 0
+    assert_equal generation.live_cells.first.y_coordinate, 0
+  end
+
+  def test_next_for_generation_with_zero_live_cells_returns_generation_with_zero_live_cells
+    grid = GameOfLife::Grid.new(3,3)
+    generation = GameOfLife::Generation.new(grid)
+    new_generation = generation.next
+    assert_equal new_generation.live_cells.count, 0
   end
 end
