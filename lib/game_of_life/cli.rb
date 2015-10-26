ac03f0a46828b462721349c6c973956e3a299a28
Chirag Aggarwal <me@chi6rag.net>
2015-10-26 12:58:06 +0530

    Adding API for reference

--- lib/game_of_life/cli.rb
+++ lib/game_of_life/cli.rb
@@ -2,8 +2,44 @@ module GameOfLife
   class Cli
     def start
       puts "Initial Configuration"
-      print "Enter the size of grid: "
-      grid_size = gets.chomp.to_i
+      print "\nEnter the length of grid: "
+      grid_length = gets.chomp.to_i
+      print "\nEnter the breadth of grid: "
+      grid_breadth = gets.chomp.to_i
+      grid = Grid.new(grid_length, grid_breadth)
+      generation = nil;
+
+      puts "\nDoes the first generation have live cells (t/f): "
+      live_cells_present = gets.chomp
+
+      if live_cells_present == "t"
+        live_cells = []
+        (grid_length * grid_breadth).times do
+          puts "\nEnter the x coordinate: "
+          x_coordinate = gets.chomp.to_i
+          puts "\nEnter the y coordinate: "
+          y_coordinate = gets.chomp.to_i
+          live_cells << Cell.new(x_coordinate, y_coordinate, true)
+        end
+        generation = Generation.new(grid, live_cells.uniq)
+      else
+        generation = Generation.new(grid)
+      end
+
+      while true
+        puts '1. See next state'
+        puts '2. Exit'
+        print "\nChoice: "
+        choice = gets.chomp.to_i
+        case choice
+        when 1:
+          generation = generation.next
+          # print generation
+        when 2:
+          System.exit(0)
+        end
+      end
+
     end
   end
 end
