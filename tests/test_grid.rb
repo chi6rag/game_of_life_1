7318ebd8defcc0616d197339ff176726a33034c1
Chirag Aggarwal <me@chi6rag.net>
2015-10-26 12:12:39 +0530

    Adding test to expect #live_cells for grid with no live cells to return nil

--- tests/test_grid.rb
+++ tests/test_grid.rb
@@ -2,9 +2,17 @@ require "./lib/game_of_life.rb"
 require "test/unit"
 
 class GridTest < Test::Unit::TestCase
-  def test_grid_with_valid_size_creates_a_new_grid
+  def test_new_with_valid_size_creates_a_new_grid
     grid = GameOfLife::Grid.new(3,3)
     assert_equal grid.class, GameOfLife::Grid
     assert_true grid.is_valid?
+  end
+
+  def test_new_with_invalid_size_raises_error
+  end
+
+  def test_live_cells_for_grid_with_no_live_cells_returns_nil
+    grid = GameOfLife::Grid.new(3,3)
+    assert_equal grid.live_cells, nil
   end
 end
