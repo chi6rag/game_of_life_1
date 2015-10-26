7a71b2b7e293e3d6a67cf4a71a749b82561e807c
Chirag Aggarwal <me@chi6rag.net>
2015-10-26 12:15:29 +0530

    Adding #live_cells method on Grid

--- lib/game_of_life/grid.rb
+++ lib/game_of_life/grid.rb
@@ -1,13 +1,18 @@
 module GameOfLife
   class Grid
-    def initialize(length, breadth)
+    def initialize(length, breadth, live_cells=nil)
       @length = length
       @breadth = breadth
+      @live_cells = live_cells
     end
 
     def is_valid?
       return true if (@length.class == Fixnum && @breadth.class == Fixnum)
       return false
+    end
+
+    def live_cells
+      @live_cells
     end
   end
 end
